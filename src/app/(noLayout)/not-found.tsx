e865538165c8f8259b105ecd548f7ee92a82910f
dylan <1611185386@qq.com>
2024-07-24 18:20:55 +0800

    feat: 修改not-found.tsx没有Layout的错误

--- src/app/(noLayout)/not-found.tsx
+++ src/app/(noLayout)/not-found.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function NotFound() {
   return (
     <div>
-      <h2>Not Found --- root</h2>
+      <h2>Not Found --- noLayout</h2>
       <p>Could not find requested resource</p>
       <Image src="/images/404.png" alt="404" layout="fill"></Image>
       <Link href="/">Return Home</Link>
